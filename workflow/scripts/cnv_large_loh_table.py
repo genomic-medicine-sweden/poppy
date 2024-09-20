0ff6bec2d0f8a1bfbb959ff124bc282a11fe4986
Arielle R Munters <arielle@reivant.se>
2024-09-20 10:34:23 +0200

    fix: update report module to include extra tables, small fixes in cnv_large_loh_table.py

--- workflow/scripts/cnv_large_loh_table.py
+++ workflow/scripts/cnv_large_loh_table.py
@@ -1,6 +1,7 @@
 import logging
 from pysam import VariantFile
-from hydra_genetics.utils.io import utils
+from operator import itemgetter
+
 
 log = logging.getLogger()
 
@@ -12,8 +13,8 @@ def create_cytocoordinates_table(in_chrom_arm_size):
     with open(in_chrom_arm_size) as file:
         for line in file:
             columns = line.strip().split("\t")
-            cyto_table.append(columns[0], columns[1], columns[2], columns[0][3:] + columns[3])
-            genome_size += columns[2] - columns[1]
+            cyto_table.append([columns[0], int(columns[1]), int(columns[2]), columns[0][3:] + columns[3]])
+            genome_size += int(columns[2]) - int(columns[1])
     return cyto_table, genome_size
 
 
@@ -24,20 +25,21 @@ def polyploidy_check(variant, normal_cn_lower_limit, normal_cn_upper_limit, norm
     size = end - start + 1
     cn = variant.info["CORR_CN"]
     BAF = variant.info["BAF"]
+
     polyploidy = 0
     if (
         cn > normal_cn_lower_limit
         and cn < normal_cn_upper_limit
         and BAF
-        and (BAF < normal_baf_lower_limit or BAF > normal_baf_upper_limit)
+        and (float(BAF) < normal_baf_lower_limit or float(BAF) > normal_baf_upper_limit)
     ):  # loh
         polyploidy += size
     elif (
-        cn > amp_chr_arm_cn_limit and BAF and (BAF > normal_baf_lower_limit and BAF < normal_baf_upper_limit)
+        cn > normal_cn_upper_limit and BAF and (float(BAF) > normal_baf_lower_limit and float(BAF) < normal_baf_upper_limit)
     ):  # amp med ingen loh
         polyploidy += size
     elif (
-        cn < del_chr_arm_cn_limit and BAF and (BAF > normal_baf_lower_limit and BAF < normal_baf_upper_limit)
+        cn < normal_cn_lower_limit and BAF and (float(BAF) > normal_baf_lower_limit and float(BAF) < normal_baf_upper_limit)
     ):  # del med ingen loh
         polyploidy += size
     return polyploidy
@@ -50,7 +52,9 @@ def write_tsv_file(outfile_tsv, data_table, genome_size, polyploidy_fraction_lim
         if polyploidy_size / genome_size > polyploidy_fraction_limit:
             outfile.write(f"Warning: potential polyploidy detected!\t\t\t\t\t\t\t")
             outfile.write(f"{polyploidy_size * 100 / genome_size:.1f}% polyploid regions\n")
-        for outline in data_table["data"]:
+
+        data_sorted = sorted(data_table["data"], key=lambda x: int(x[1][3:]) if x[1][3:].isnumeric() else 100)
+        for outline in data_sorted:
             outfile.write("\t".join(outline) + "\n")
 
 
@@ -72,34 +76,56 @@ def create_tsv_report(
 
     polyploidy_size = 0
     large_table = {
-        "header": ["CytoCoord", "Chr", "Start", "End", "Gene(s)", "Caller", "Type", "CN", "BAF", "NormalAF"],
+        "header": ["CytoCoord", "Chr", "Start", "End", "Caller", "Type", "CN", "BAF", "NormalAF", "Gene(s)"],
         "data": [],
     }
-    loh_table = {"header": ["CytoCoord", "Chr", "Start", "End", "Gene(s)", "Caller", "Type", "CN", "BAF", "NormalAF"], "data": []}
+    loh_table = {
+        "header": ["CytoCoord", "Chr", "Start", "End", "Caller", "Type", "CN", "BAF", "NormalAF", "Gene(s)"],
+        "data": [],
+    }
     for variant in variants_file.fetch():
         cyto_coordinates = ""
-        BAF = float(variant.info["BAF"]) if variant.info["BAF"] else "NA"
-        normal_af = float(variant.info["Normal_AF"]) if variant.info["Normal_AF"] else 0.0
+        BAF = variant.info["BAF"]
+
+        try:
+            if isinstance(variant.info["Genes"], tuple):
+                genes = ",".join(variant.info["Genes"])
+            else:
+                genes = variant.info["Genes"]
+        except KeyError:
+            genes = ""
+
+        try:
+            normal_af = float(variant.info["Normal_AF"])
+        except KeyError:
+            log.debug(f"No Normal_AF was found setting to None. {variant.contig}:{variant.pos}")
+            normal_af = None
+
         variant_end = variant.pos + int(variant.info["SVLEN"]) - 1
 
         for cyto_line in cyto_table:
             if variant.contig == cyto_line[0] and (
                 (variant.pos <= cyto_line[2] and variant.pos >= cyto_line[1])
-                or (variant_end <= cyto_line[2] and variant.pos >= cyto_line[1])
             ):
-                cyto_coordinates.append(cyto_line[3])
+                cyto_coordinates = cyto_line[3]
+            elif variant.contig == cyto_line[0] and (
+                (variant_end <= cyto_line[2] and variant_end >= cyto_line[1])
+            ):
+                cyto_coordinates += "-" + cyto_line[3]
 
         table_line = [
             cyto_coordinates,
             variant.contig,
-            variant.pos,
-            variant_end,
-            variant.info["Genes"],
-            variant.info["Caller"],
-            variant.info["CORR_CN"],
-            BAF,
-            normal_af,
+            str(variant.pos),
+            str(variant_end),
+            variant.info["CALLER"],
+            variant.info["SVTYPE"] if "NORMAL" not in variant.info["SVTYPE"] else "NORMAL",
+            str(round(variant.info["CORR_CN"], 3)),
+            str(round(BAF, 2)) if BAF else "",
+            str(round(normal_af, 2)) if normal_af else "",
+            genes,
         ]
+
         # polyploidy check
         if variant.info["CALLER"] == "cnvkit":
             polyploidy_size += polyploidy_check(
@@ -110,17 +136,16 @@ def create_tsv_report(
         if (
             (variant.info["CORR_CN"] < normal_cn_lower_limit or variant.info["CORR_CN"] > normal_cn_upper_limit)
             and int(variant.info["SVLEN"]) > min_detection_size
-            and variant.info["Normal_AF"] < 0.15
-        ):  # tar val alla just nu, borde bara vara del dup?
-            # table_line: cyto chr start end genes, caller, type, cn, baf, normal af
+            and (not normal_af or normal_af < 0.15)
+        ):
             large_table["data"].append(table_line)
 
         # loh table
         elif (
             variant.info["CORR_CN"] > normal_cn_lower_limit
             and variant.info["CORR_CN"] < normal_cn_upper_limit
-            and BAF != "NA"
-            and (BAF < normal_baf_lower_limit or BAF > normal_baf_upper_limit)
+            and BAF
+            and (float(BAF) < normal_baf_lower_limit or float(BAF) > normal_baf_upper_limit)
         ):
             loh_table["data"].append(table_line)
 
@@ -129,15 +154,6 @@ def create_tsv_report(
     log.info(f"Processed all variants, starting to write to outputs")
     write_tsv_file(large_tsv, large_table, genome_size, polyploidy_fraction_limit, polyploidy_size)
     write_tsv_file(loh_tsv, loh_table, genome_size, polyploidy_fraction_limit, polyploidy_size)
-
-    log.info(f"Processed all variants, starting to write to {loh_tsv}")
-    with open(loh_tsv, "w+") as outfile:
-        outfile.write("\t".join() + "\n")
-        if polyploidy_size / genome_size > polyploidy_fraction_limit:
-            outfile.write(f"Warning: potential polyploidy detected!\t\t\t\t\t\t\t")
-            outfile.write(f"{polyploidy_size * 100 / genome_size:.1f}% polyploid regions\n")
-        for outline in loh_table:
-            outfile.write("\t".join(outline) + "\n")
 
 
 if __name__ == "__main__":
@@ -160,10 +176,10 @@ if __name__ == "__main__":
         out_loh,
         g_size,
         cyto_table,
-        normal_cn_lower_limit,
-        normal_cn_upper_limit,
-        normal_baf_lower_limit,
-        normal_baf_upper_limit,
-        polyploidy_fraction_limit,
-        min_detection_size,
+        float(normal_cn_lower_limit),
+        float(normal_cn_upper_limit),
+        float(normal_baf_lower_limit),
+        float(normal_baf_upper_limit),
+        float(polyploidy_fraction_limit),
+        int(min_detection_size),
     )
